6fe4214572d9344265bdcac2de4f233b3122c223
Chontinee-tnk <67319010043@tnk.ac.th>
2024-11-14 08:35:34 +0700

    ex1.py

--- python_week5/ex1.py
+++ python_week5/ex1.py
@@ -1,31 +1,16 @@
-import ex
+def test(a,b):
+  result = []
+  for i in range(a, b+ 1):
+      if i %3 != 0:
+          result.append(i)
+  return result
 
-while True:
-    print('\nโปรแกรมที่ต้องการใช้')
-    print("1 = %3 , 2 = รวมค่า")
-    a = int(input('เลือก : '))
-    if a == 1:
-        while True:
-            print('\nโปรแกรม %3')
-            print('พิมพ์ 0 เพื่อต้องการออก')
-            a = 1
-            b = int(input('ใส่ค่าปลาย : '))    
-            c = ex.ex(a,b)
-            if b == 0:
-                break
-            print(f'{c}\n')
-
-    elif a == 2 :
-        b = 0
-        c = 0
-        while True:
-            print('\nโปรแกรมรวมค่า')
-            print('พิมพ์ 0 เพื่อต้องการออก')
-            a = int(input('ใส่เลข : '))
-            if a == 0:
-                print(f'ผลรวมของค่าบวก = {b}')
-                print(f'ผลรวมของค่าลบ = {c}')
-                break
-            b,c = ex.sum(a,b,c)
-            print(b,c)
-    
+def sum(nums,sum1,sum2):
+  while True:
+    if nums > 0:
+        sum1 += nums
+    elif nums < 0:
+        sum2 += nums
+    else:
+        break
+    return sum1, sum2
